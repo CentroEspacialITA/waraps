c976580cb7774b985f1bde5e17ca872b7c0b383b
Matheus V. C. Cogo <mvccogo@gmail.com>
2023-08-04 17:12:38 -0300

    Change rosbridge fork (still got to apply fixes)

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/opt/lrs2/install/local_setup.bash"
+source "/opt/conceptio/rosbridge-suite/install/local_setup.bash"
+echo "\n\t\t***CONCEPTIO-LRS2 Docker***\nIn order to open more terminals, run the following docker command:\ndocker exec -ti <container_name> bash\n"
 exec "$@"
