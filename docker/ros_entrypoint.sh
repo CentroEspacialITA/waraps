e7428d75f24380e35b384c7fc8fc51815771e37c
Matheus V. C. Cogo <mvccogo@gmail.com>
2023-08-07 13:45:40 -0300

    Fix dockerfile+entrypoint

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/opt/lrs2/install/local_setup.bash"
-source "/opt/conceptio/rosbridge-suite/install/local_setup.bash"
-echo "\n\t\t***CONCEPTIO-LRS2 Docker***\nIn order to open more terminals, run the following docker command:\ndocker exec -ti <container_name> bash\n"
+source "/opt/conceptio/rosbridge_suite/install/local_setup.bash"
+GREEN=$(tput setaf 2)
+CYAN=$(tput setaf 6)
+NC=$(tput sgr0)
+
+
+echo -e -n "\n\t\t ${GREEN}*** CONCEPTIO-LRS2 Docker *** ${CYAN}\nIn order to open more terminals, run the following docker command:\ndocker exec -ti -p PORT_RANGE:PORT_RANGE <container_name> bash\n ${NC}"
 exec "$@"
